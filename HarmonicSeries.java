9990511f1f3c226814af958148162aa781e39ab1
Medini17 <medinipd@gmail.com>
2018-05-19 15:04:35 +0530

    asr

---
+++ HarmonicSeries.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.functionalprograms;
+
+import com.bridgelabz.utility.Utility;
+
+/**
+ * Created By:Medini P.D
+ * Date:- 18/05/2018
+ * Purpose:Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N
+ */
+public class HarmonicSeries {
+
+	public static double harmonic(int n)
+	{
+	  double sum=0.0;
+	    if(n!=0)
+	  {
+	     for(double i=1; i<=n; i++)
+	   {
+	     sum=sum+(1/i);
+	   }
+	}
+	  return sum;
+	  
+	}
+	public static void main(String[] args)
+	{
+		Utility u=new Utility();
+		System.out.println("enter the input");
+	 int n = u.inputInteger();
+      double res=harmonic(n);
+	  System.out.println("Nth value is=" +res);
+	}
+	}
---
+++ PowerOf2.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.functionalprograms;
+
+import com.bridgelabz.utility.Utility;
+
+/**
+ * Created By:Medini P.D
+ * Date:- 18/05/2018
+ * Purpose:This program takes a command-line argument N and prints a 
+ * table of the powers of 2 that are less than or equal to 2^N
+ */
+public class PowerOf2 {
+
+	public static void main(String[] args) 
+	{
+		Utility u=new Utility();
+		System.out.println("Enter a number:");
+        int n1=u.inputInteger();
+        int res=u.power(n1);
+        System.out.println(res);
+	}
+}
