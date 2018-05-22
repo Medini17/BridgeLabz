c239da6d75ead087a477c90f48f99bffed3aa2f9
Medini17 <medinipd@gmail.com>
2018-05-22 17:18:23 +0530

    asr

--- PrimeFactor.java
+++ PrimeFactor.java
@@ -8,34 +8,34 @@ import com.bridgelabz.utility.Utility;
  */
 public class PrimeFactor
 {
-	public static boolean isPrime(int i)
-	{
-		for(int j=2;j<i;j++)
+	  public static boolean isPrime(int i)
 		{
-			if(i%j==0)
+			for(int j=2;j<i;j++)
 			{
-	     		return false;
-	    	}
-	  	}
-	 	return true;
-	 }
+				if(i%j==0)
+				{
+		     		return false;
+		    	}
+		  	}
+		 	return true;
+		 }
 
-	public static void main(String[] args) 
-	{ 
-        Utility u=new Utility();
-		System.out.println("Enter the no. to find PrimeFactors:");
-		int n=u.inputInteger();
-		System.out.println();
-		System.out.println("The prime factors for the "+n+" is:");
-		System.out.println();
-		   
-		for(int i=2;i<=n;i++)
-		{  
-			if(isPrime(i) && n%i==0)
-			{
-				System.out.print(i+" ");
+		public static void main(String[] args) 
+		{ 
+	        Utility u=new Utility();
+			System.out.println("Enter the no. to find PrimeFactors:");
+			int n=u.inputInteger();
+			System.out.println();
+			System.out.println("The prime factors for the "+n+" is:");
+			System.out.println();
+			   
+			for(int i=2;i<=n;i++)
+			{  
+				if(isPrime(i)&& (n%i==0))
+				{
+					System.out.print(i+" ");
+				}
 			}
-		}
-        System.out.println();
-	}
+	        System.out.println();
+		}	
 }
