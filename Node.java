ee1ff5fcfb442df1a6b27a635d2be528a5fe3687
Medini17 <medinipd@gmail.com>
2018-05-25 13:20:18 +0530

    asr

---
+++ Node.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.bridgelabz.task;
+
+/**
+ * Created By:Medini P.D
+ * Date:- 19/05/2018
+ * 
+ */
+
+
+public class Node<E>
+{
+	public String data1;
+	public int data;
+	public Node nextNode;
+	
+	//constructor
+	public Node()
+	{
+		this.nextNode=null;
+	}
+
+	public void setData(String data1)
+	{
+		this.data1=data1;
+		nextNode=null;
+	}
+	
+	public void setData(int data)
+	{
+		this.data=data;
+		nextNode=null;
+	}
+}
+
