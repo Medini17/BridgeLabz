fc3167e05a29a34d1f011f1a85d6f00d3ba531d3
Medini <medinipd@gmail.com>
2018-05-28 11:20:50 +0530

    asd

--- CouponNumber.java
+++ CouponNumber.java
@@ -2,43 +2,40 @@ package com.bridgelabz.functionalprograms;
 
 import com.bridgelabz.utility.Utility;
 
-/**
+/**-----------------------------------------------------------------------------------------------
  * Created By:Medini P.D
  * Date:- 18/05/2018
  * Purpose: Given   N   distinct   Coupon   Numbers,   how   many   random   numbers   do   you
  * need   to   generate   distinct   coupon   number?   This   program   simulates   this   random
  * process.
- */
+ *--------------------------------------------------------------------------------------------*/
 public class CouponNumber {
 	public static void main(String[] args) {
-		Utility u=new Utility();
-		int a=0;
-		int n=u.inputInteger();
+		Utility u = new Utility();
+		int a = 0;
+		int n = u.inputInteger();
 
-		int noOftimes=0;
-		while(a<n)
-		{
-		int max = 1000;
-		char[] M = "abcdABCD12347".toCharArray();
-		
-		StringBuilder sb = new StringBuilder();
+		int noOftimes = 0;
+		while (a < n) {
+			int max = 10000;
+			char[] M = "abcdABCD12347".toCharArray();
 
-		
-	
-		int random = (int) (Math.random() * max);
-		
+			StringBuilder sb = new StringBuilder();
 
-		while (random > 0) {
-			sb.append(M[random % M.length]);
-			random = random / M.length;
-			noOftimes++;
+			int random = (int) (Math.random() * max);
+
+			while (random > 0) {
+				sb.append(M[random % M.length]);
+				random = random / M.length;
+				noOftimes++;
+			}
+
+			String couponCode = sb.toString();
+			a++;
+
+			System.out.println("Coupon Code: " + couponCode);
+
 		}
-		
-		String couponCode = sb.toString();
-		a++;
-		
-		System.out.println("Coupon Code: " + couponCode);
-		
-	}
 		System.out.println("no of times taken to genarate required no of coupon codes: " + noOftimes);
-}}
+	}
+}
