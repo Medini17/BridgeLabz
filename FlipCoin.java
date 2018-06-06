9fdfb1e625c277cbfda78e4fd9e8388e38f85d44
Medinidath17 <medinipd@gmail.com>
2018-06-06 10:17:06 +0530

    asr

--- FlipCoin.java
+++ FlipCoin.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.functionalprograms;
 
+import java.util.Scanner;
+
 import com.bridgelabz.utility.Utility;
 
 /**
@@ -8,24 +10,42 @@ import com.bridgelabz.utility.Utility;
  * Purpose:The   number   of   times   to   Flip   Coin.   Ensure
  * it   is   positive   integer  and to tell heads or tails!. 
  */
+
 public class FlipCoin {
 
+	int heads = 0; // heads count
+
 	public static void main(String[] args) {
-		CoinToss test = new CoinToss();
-		int choice;
-		System.out.println("Welcome to the coin flip game!");
 
-		do {
-			System.out.println("Enter '1' to Flip Coin OR Enter '0' to Quit: ");
-			Utility u = new Utility();
-			choice = u.inputInteger();
+		FlipCoin flipCoin = new FlipCoin(); // class object
+		flipCoin.start();
+	}
 
-			if (choice == 1) {
-				test.flip();
-			} else if (choice > 1) {
-				System.out.println("Invalid entry - please enter 1 or 0: ");
-				choice = u.inputInteger();
+	void start() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter number of flips:");
+		int numberOfFlips = scanner.nextInt();	//	stores number of flips
+		scanner.close();
+
+		if (numberOfFlips < 1) {	//	negative number or 0
+			System.out.println("Number must be greater than 0");
+			start();
+		}
+
+		for (int i = 0; i < numberOfFlips; i++) {
+			double randomValue = Math.random();
+			if (randomValue < 0.5) {	//	tails
+				System.out.print("Tails\t");
+			} else {	//	it's heads, increases heads count
+				System.out.print("Heads\t");
+				heads++;
 			}
-		} while (choice != 0);
+		}
+
+		//	prints heads and tails percentages
+		System.out.println();
+		System.out.println("Heads Percentage: " + (double) heads / numberOfFlips);
+		System.out.println("Tails Percentage: " + ((double) numberOfFlips - heads) / numberOfFlips);
+
 	}
 }
